f590fd2f3fdc431dcf7567cf8e6e50f5c2c1cf69
Ibrahim Gazi <coder.ibrahimgazi@gmail.com>
2025-11-01 12:34:05 +0600

    create user API for backend

---
+++ backend/server.js
@@ -0,0 +1,44 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./DB/connectDB.js";
+import authRoutes from "./routes/authRoutes.js";
+
+dotenv.config();
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.json());
+app.use(
+    cors({
+        origin: ["http://localhost:8080", "https://BuilderX.vercel.app"],
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true,
+    })
+);
+
+app.get("/", (req, res) => {
+    res.json("Backend Running");
+});
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Node Server Running In ${process.env.NODE_ENV} Mode On Port ${PORT}`);
+});
+
+
+if (process.env.NODE_ENV !== "production") {
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+        console.log(
+            `✅ Node Server Running In ${process.env.NODE_ENV} Mode on Port ${PORT}`
+        );
+    });
+}
+
+export default app;
+
